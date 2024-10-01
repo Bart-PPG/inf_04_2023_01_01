8b5bf8757296ab9f9b8cdcefc189a95b93c36b07
Bart-PPG <Bartek.nitek@onet.pl>
2024-10-01 18:48:26 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
-
+//https://github.com/Technikum-TEB-Edukacja-we-Wroclawiu/INF.04-rozwiazania/blob/main/_arkusze/2023-01/inf_04_2023_01_02_AG_kolor.pdf
 
     public static void main(String[] args) {
         Notatka notatka = new Notatka("Tytuł notatki", "tresc notatki");
